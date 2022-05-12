2f654c875371683a6251fe67efe3ce6eb43fc853
dtaggart1 <101219151+dtaggart1@users.noreply.github.com>
2022-05-12 14:31:28 -0400

    coding challenge to remove spaces from a string

---
+++ cw5-12/removeStringSpaces.js
@@ -0,0 +1,6 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+const removeStringSpaces = str => str.replace(/ /g, '')
+
+console.log(removeStringSpaces('Remove spaces from this string'))
+ //Removespacesfromthisstring
