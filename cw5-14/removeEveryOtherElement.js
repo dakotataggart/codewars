7ef3f783e8418c8d340384250ddfc85bc5667b04
dtaggart1 <101219151+dtaggart1@users.noreply.github.com>
2022-05-15 01:38:47 -0400

    made a working solution for this codewars problem

--- cw5-14/removeEveryOtherElement.js
+++ cw5-14/removeEveryOtherElement.js
@@ -5,5 +5,12 @@
 // ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
 
 function removeEveryOther(arr){
-    return arr.filter(element => element.indexOf() % 2 == 0);
+    return arr.filter(function(value, index){
+        return index % 2 === 0;
+    })
 }
+
+//so I learned that you can reference "value" without needing to call it. I'm sure there's a reason why.
+
+
+//Wait I think I know the reason. It's the "index" OF THAT "value", so technically the value is getting referenced?
