a61f6d2ea53aede2e2edb2a955e39ac3c0c4616a
dtaggart1 <101219151+dtaggart1@users.noreply.github.com>
2022-05-18 13:50:00 -0400

    Add coding challenge from 5-18, struggled through edge cases

---
+++ cw5-18/countPositiveSumNegatives.js
@@ -0,0 +1,32 @@
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+  if (input == null || input.length === 0) {
+    return [];
+  } else {
+    let count = 0;
+    let sum = 0;
+    // iterate through each value
+    input.forEach((element) => {
+      if (element > 0) {
+        count += 1; // if positive, add to count
+      } else if (element < 0) {
+        sum += element; // if negative, add to sum
+      }
+    });
+    // return [counter, sum]
+    return [count, sum];
+  }
+}
+
+const test = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+// const test = []
+// const test = null
+console.log(countPositivesSumNegatives(test));
