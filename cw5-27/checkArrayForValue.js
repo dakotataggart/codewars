497f7ed5bbf0bef70cbf9b17dc4254a732ddd1a5
dtaggart1 <101219151+dtaggart1@users.noreply.github.com>
2022-05-28 00:17:01 -0400

    Added comments including various other ways of solving

--- cw5-27/checkArrayForValue.js
+++ cw5-27/checkArrayForValue.js
@@ -8,5 +8,56 @@ const check = (a, x) => a.includes(x) ? true : false;
 
 const testArray = [101, 45, 75, 105, 99, 107];
 const testValue = 108; // false
-const testValue = 107; // true
+const testValue2 = 107; // true
 console.log(check(testArray, testValue))
+
+
+//// Borrowed the part below from a very thorough solution on codewars by a user named Shadow_Cat
+
+/*
+  (4) Eliminate all the overhead setup.
+      Use the ES2015 SOME method to see if one or more
+      of the array entries matches our target.
+      Written in the form of a lambda (arrow function) to make it streamlined
+    
+    const check = (a,x)=>a.some(v=>v==x)
+*/
+
+/*
+(3) Filter the Array
+    Reduce it down to matching values or an empty set ... [66]
+    Test the length of the array   [66].length > 0
+
+  function check(a,x){
+    return (a.filter(v=>v==x)).length > 0
+  }
+*/
+
+/*
+(2) Walk the array without ability to break the loop.
+    Create a variable to hold the state of our match.
+    Check if the target is found and return the findings
+
+  function check(a,x){
+    let foundTarget = false
+    a.forEach((v,i,A)=>{
+      if (v==x){ foundTarget = true}
+    })
+    return foundTarget
+  }
+*/
+
+
+/*
+  (1) Manually Walk the array, 
+    Break out of the loop (return true) if you find a match
+    otherwise return false
+
+  function check(a,x){
+    for (let i=0; i<a.length;i++){
+      console.log(a[i])
+      if (a[i]==x){ return true }
+    }
+    return false
+  }
+*/
