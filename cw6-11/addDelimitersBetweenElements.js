04cd1a7221c9ba098eae75b4adae736b6a540e60
dtaggart1 <101219151+dtaggart1@users.noreply.github.com>
2022-06-11 18:25:38 -0400

    Add coding challenge to join array elements with comma delimiter

---
+++ cw6-11/addDelimitersBetweenElements.js
@@ -0,0 +1,15 @@
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in the same order.
+
+// "h,o,l,a"
+
+const printArray = array => array.join()
+
+let test = [2,4,5,2];
+
+console.log(printArray(test));
+
+// Understood the assignment. Pretty sure this is just the simplest method of completing the given task
