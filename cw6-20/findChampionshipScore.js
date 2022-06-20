9bd152e5e388a107d69a92666bc993159b351e2e
dtaggart1 <101219151+dtaggart1@users.noreply.github.com>
2022-06-20 00:29:57 -0400

    Finished coding challenge

--- cw6-20/findChampionshipScore.js
+++ cw6-20/findChampionshipScore.js
@@ -14,7 +14,24 @@
 // 0 <= y <= 4
 
 const points = array => {
-  array.forEach()
+  let total = 0 
+  array.forEach((element) => {
+    if (Number(element[0]) > Number(element[2])){
+      total += 3
+    }else if (Number(element[0]) === Number(element[2])){
+      total += 1
+    }
+  });
   // get number value of each pair
   // compare values in pairs + assign them result value (0/1/3)
+    console.log(total)
 }
+
+const test = ['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3']
+const test2 = ['1:1','2:2','3:3','4:4','2:2','3:3','4:4','3:3','4:4','4:4']
+points(test)
+points(test2)
+
+
+// ASSIGNMENT != EQUIVALENCE 
+// Other than that, got it all on my own. We love that 
